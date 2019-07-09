63a81a124c5ddffeb0e3f84b02c37eb13e06ec1f
Tharindu Munasinghe <tharindu.munasinghe@trycake.com>
2019-07-09 15:55:28 +0530

    removed the runit dependencies and install systemd

--- image/system_services.sh
+++ image/system_services.sh
@@ -4,10 +4,6 @@ source /bd_build/buildconfig
 set -x
 
 ## Install init process.
-cp /bd_build/bin/my_init /sbin/
-mkdir -p /etc/my_init.d
-mkdir -p /etc/my_init.pre_shutdown.d
-mkdir -p /etc/my_init.post_shutdown.d
 mkdir -p /etc/container_environment
 touch /etc/container_environment.sh
 touch /etc/container_environment.json
@@ -21,9 +17,6 @@ ln -s /etc/container_environment.sh /etc/profile.d/
 ## Install runit.
 apt-get install -y systemd
 
-$minimal_apt_get_install runit
-## Install a syslog daemon and logrotate.
-[ "$DISABLE_SYSLOG" -eq 0 ] && /bd_build/services/syslog-ng/syslog-ng.sh || true
 
 ## Install the SSH servkkjer.
 [ "$DISABLE_SSH" -eq 0 ] && /bd_build/services/sshd/sshd.sh || true
